bc4f8b45e7efddec10c49627d1c7c8e119d8eb84
Leslie Iorcansky <leslieior@hotmail.com>
2022-08-25 18:16:30 -0300

    creando algoritmo para un ciclo

---
+++ main.js
@@ -0,0 +1,6 @@
+let ingresarNumero = Number(prompt('Ingrese un número'));
+
+for (let i=1; i <= 10; i++) {
+    let resultado =  ingresarNumero + i;
+    alert ('El resultado de: ' + ingresarNumero + ' + ' +i+ '='+ resultado);
+}
